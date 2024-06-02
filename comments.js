933e36684b924e693862dabdb891fc40690791c5
shui33 <104038769+shui33@users.noreply.github.com>
2024-06-02 21:14:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,96 +1,29 @@
 //create web server
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
 
 //create a route
-app.get('/comments', function(req, res) {
-  res.send('This is the comments page');
+app.get('/', (req, res) => {
+    res.send("Hello World");
 });
-
-//start the server
-app.listen(3000, function() {
-  console.log('Server is running on http://localhost:3000');
-});
-
-// Path: index.js
-//create web server
-var express = require('express');
-var app = express();
 
 //create a route
-app.get('/', function(req, res) {
-  res.send('This is the home page');
+app.get('/api/comments', (req, res) => {
+    res.send([1,2,3]);
 });
-
-//start the server
-app.listen(3000, function() {
-  console.log('Server is running on http://localhost:3000');
-});
-
-// Path: app.js
-//create web server
-var express = require('express');
-var app = express();
 
 //create a route
-app.get('/', function(req, res) {
-  res.send('This is the home page');
+app.get('/api/comments/:id', (req, res) => {
+    res.send(req.params.id);
 });
-
-app.get('/comments', function(req, res) {
-  res.send('This is the comments page');
-});
-
-//start the server
-app.listen(3000, function() {
-  console.log('Server is running on http://localhost:3000');
-});
-
-// Path: app.js
-//create web server
-var express = require('express');
-var app = express();
 
 //create a route
-app.get('/', function(req, res) {
-  res.send('This is the home page');
+app.get('/api/comments/:year/:month', (req, res) => {
+    res.send(req.params);
 });
-
-app.get('/comments', function(req, res) {
-  res.send('This is the comments page');
-});
-
-app.get('/about', function(req, res) {
-  res.send('This is the about page');
-});
-
-//start the server
-app.listen(3000, function() {
-  console.log('Server is running on http://localhost:3000');
-});
-
-// Path: app.js
-//create web server
-var express = require('express');
-var app = express();
 
 //create a route
-app.get('/', function(req, res) {
-  res.send('This is the home page');
+app.get('/api/comments/:year/:month', (req, res) => {
+    res.send(req.query);
 });
-
-app.get('/comments', function(req, res) {
-  res.send('This is the comments page');
-});
-
-app.get('/about', function(req, res) {
-  res.send('This is the about page');
-});
-
-app.get('/contact', function(req, res) {
-  res.send('This is the contact page');
-});
-
-//start the server
-app.listen(3000, function() {
-  console
